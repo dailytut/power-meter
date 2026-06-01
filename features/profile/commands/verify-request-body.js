712798adff812290a5da5c21c5d341a0f6229bed
dailytut <blog@dailytut.com>
2026-06-01 16:25:19 +0530

    update Joi validation to modern API

--- features/profile/commands/verify-request-body.js
+++ features/profile/commands/verify-request-body.js
@@ -4,41 +4,25 @@ const constants = require('../constants');
 
 const { NAME_MIN, NAME_MAX } = constants;
 
-const schema = Joi.object().keys({
-  name: Joi.string()
-    .min(NAME_MIN)
-    .max(NAME_MAX)
-    .required(),
-  username: Joi.string().email({ minDomainAtoms: 2 }),
+const schema = Joi.object({
+  name: Joi.string().min(NAME_MIN).max(NAME_MAX).required(),
+  username: Joi.string().email({ tlds: { allow: true } }),
 });
 
-async function validateRegisterPayload(req, res, next) {
-  let payloadValidation;
-  try {
-    payloadValidation = await Joi.validate(req.body, schema, { abortEarly: false });
-  } catch (validateRegisterError) {
-    payloadValidation = validateRegisterError;
-  }
-  const { details } = payloadValidation;
-  let errors;
-  if (details) {
-    errors = {};
-    details.forEach(errorDetail => {
-      const {
-        message,
-        path: [key],
-        type,
-      } = errorDetail;
+async function validateProfilePayload(req, res, next) {
+  const { error } = schema.validate(req.body, { abortEarly: false });
+
+  if (error) {
+    const errors = {};
+    error.details.forEach(({ message, path: [key], type }) => {
       const errorType = type.split('.')[1];
       errors[key] = constants[`${key.toUpperCase()}_${errorType.toUpperCase()}_ERROR`] || message;
     });
-  }
-
-  if (errors) {
     req.session.messages = { errors };
     return res.status(400).redirect('/profile');
   }
+
   return next();
 }
 
-module.exports = validateRegisterPayload;
+module.exports = validateProfilePayload;
--- features/register/commands/verify-request-body.js
+++ features/register/commands/verify-request-body.js
@@ -4,42 +4,25 @@ const constants = require('../constants');
 
 const { NAME_MIN, NAME_MAX, PASSWORD_MAX, PASSWORD_MIN } = constants;
 
-const schema = Joi.object().keys({
-  name: Joi.string()
-    .min(NAME_MIN)
-    .max(NAME_MAX)
-    .required(),
-  password: Joi.string()
-    .min(PASSWORD_MIN)
-    .max(PASSWORD_MAX),
-  username: Joi.string().email({ minDomainAtoms: 2 }),
+const schema = Joi.object({
+  name: Joi.string().min(NAME_MIN).max(NAME_MAX).required(),
+  password: Joi.string().min(PASSWORD_MIN).max(PASSWORD_MAX),
+  username: Joi.string().email({ tlds: { allow: true } }),
 });
 
 async function validateRegisterPayload(req, res, next) {
-  let payloadValidation;
-  try {
-    payloadValidation = await Joi.validate(req.body, schema, { abortEarly: false });
-  } catch (validateRegisterError) {
-    payloadValidation = validateRegisterError;
-  }
-  const { details } = payloadValidation;
-  let errors;
-  if (details) {
-    errors = {};
-    details.forEach(errorDetail => {
-      const {
-        path: [key],
-        type,
-      } = errorDetail;
+  const { error } = schema.validate(req.body, { abortEarly: false });
+
+  if (error) {
+    const errors = {};
+    error.details.forEach(({ path: [key], type }) => {
       const errorType = type.split('.')[1];
       errors[key] = constants[`${key.toUpperCase()}_${errorType.toUpperCase()}_ERROR`];
     });
-  }
-
-  if (errors) {
     req.session.messages = { errors };
     return res.status(400).redirect('/register');
   }
+
   return next();
 }
 
